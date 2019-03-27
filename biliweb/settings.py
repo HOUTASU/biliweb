89dd704a7416f8b06f47310109e1b518b54a38f2
Song Wei <237298062@qq.com>
2019-03-27 20:16:08 +0800

    3/27 20:15 添加按钮及弹窗

--- biliweb/settings.py
+++ biliweb/settings.py
@@ -53,8 +53,7 @@ ROOT_URLCONF = 'biliweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,7 +79,7 @@ DATABASES = {
         'NAME': 'biliweb',
         'USER': 'root',
         'PASSWORD': '1428850347a+',
-        'HOST': 'cdb-g6zxb4ma.cd.tencentcdb.com',
+        'HOST': '119.27.176.229',
         'PORT': '10012'
     }
 }
